50dcdcdca95b43b7d20cb0c722425db87039ab1f
agusbw <agusbudhiarta81@gmail.com>
2023-11-29 17:45:20 +0800

    refactor: simplify auth middlewar

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -1,25 +1,37 @@
 import jwt from "jsonwebtoken";
 import express from "express";
 import { RequestWithToken } from "../utils/types";
+import { Role } from "@prisma/client";
 
-const authMiddleware = async (
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
-) => {
-  const token = req.header("Authorization")?.replace("Bearer ", "");
+const authMiddleware =
+  (roleNeeded: Role) =>
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const token = req.header("Authorization")?.replace("Bearer ", "");
 
-  if (!token) {
-    return res.status(401).send("Access Denied");
-  }
+    if (!token) {
+      return res.status(401).send("Access Denied");
+    }
 
-  try {
-    const decoded = jwt.verify(token, process.env.TOKEN_SECRET as string);
-    (req as RequestWithToken).token = decoded;
-    next();
-  } catch (error) {
-    res.status(401).send("Access Denied");
-  }
-};
+    try {
+      const decoded = jwt.verify(token, process.env.TOKEN_SECRET as string);
+      (req as RequestWithToken).token = decoded;
+      const { role: userRole } = decoded as { role: Role };
+
+      //admin just go through
+      if (userRole === "ADMIN") {
+        next();
+        return;
+      }
+
+      //if user role is not the same as role needed, then access denied
+      if (userRole !== roleNeeded) {
+        return res.status(401).send("Access Denied");
+      }
+
+      next();
+    } catch (error) {
+      res.status(401).send("Access Denied");
+    }
+  };
 
 export default authMiddleware;
--- src/router/protected-api.ts
+++ src/router/protected-api.ts
@@ -1,15 +1,9 @@
 import express, { Router } from "express";
 import userController from "../controller/user-controller";
 import authMiddleware from "../middleware/auth-middleware";
-import adminMiddleware from "../middleware/admin-middleware";
 
 const userRouter: Router = express.Router();
 
-userRouter.get(
-  "/api/users",
-  authMiddleware,
-  adminMiddleware,
-  userController.adminOnly
-);
+userRouter.get("/api/users", authMiddleware("USER"), userController.adminOnly);
 
 export default userRouter;
